58b09a3e7055c7f7f9da3130a84228ffaa62ef6f
Rob Miguel <rob.m@owletcare.com>
2018-06-24 22:26:00 -0600

    Initial Commit

---
+++ Shake Jack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shake Jack
+//
+//  Created by Rob Miguel on 6/24/18.
+//  Copyright © 2018 robmgl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
